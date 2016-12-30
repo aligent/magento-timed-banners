67a068e63882e723fafe6f3929a6f7a89a0094da
Todd Hainsworth <todd.hainsworth@aligent.com.au>
2016-12-30 11:14:18 +1030

    RE #230: Add template rewrite to show the holiday notice if enabled

--- app/code/community/Aligent/TimedBanners/Helper/Data.php
+++ app/code/community/Aligent/TimedBanners/Helper/Data.php
@@ -1,2 +1,48 @@
 <?php
-class Aligent_TimedBanners_Helper_Data extends Mage_Core_Helper_Abstract { }
+/**
+ * Class Aligent_TimedBanners_Helper_Data
+ */
+class Aligent_TimedBanners_Helper_Data extends Mage_Core_Helper_Abstract {
+    const CONFIG_PREFIX = 'aligent/holiday_banner/';
+
+    /**
+     * @return bool
+     */
+    public function shouldShowBanner()
+    {
+        return Mage::getStoreConfig(self::CONFIG_PREFIX . 'enabled') && $this->isHoliday();
+    }
+
+    /**
+     * @return string
+     */
+    public function getHolidayMessage() {
+        return $this->__(Mage::getStoreConfig(self::CONFIG_PREFIX . 'message', Mage::app()->getStore()));
+    }
+
+    /**
+     * @return DateTime
+     */
+    protected function getHolidayStartDate()
+    {
+        return DateTime::createFromFormat('d/m/y', Mage::getStoreConfig(self::CONFIG_PREFIX . 'start_date'));
+    }
+
+    /**
+     * @return DateTime
+     */
+    protected function getHolidayEndDate()
+    {
+        return DateTime::createFromFormat('d/m/y', Mage::getStoreConfig(self::CONFIG_PREFIX . 'end_date'));
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isHoliday()
+    {
+        $oCurrentDate = new DateTime();
+        return $oCurrentDate >= $this->getHolidayStartDate() && $oCurrentDate <= $this->getHolidayEndDate();
+    }
+
+}
